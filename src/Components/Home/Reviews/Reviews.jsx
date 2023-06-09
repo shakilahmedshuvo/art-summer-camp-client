be86fc65acd5b24f610d37d1da0b4fd9dd3644f4
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-09 16:45:33 +0600

    add changes

--- src/Components/Home/Reviews/Reviews.jsx
+++ src/Components/Home/Reviews/Reviews.jsx
@@ -18,9 +18,12 @@ const Reviews = () => {
     return (
         <div
             className="max-w-7xl mx-auto my-20">
-            <h2>
-                WHAT PEOPLE SAY
+            <h2 className="text-4xl text-center font-bold py-4">
+                WHAT PEOPLE SAY About Us
             </h2>
+            <p className="text-center font-bold p-4">
+                Customer Review Section
+            </p>
             <div
                 className="mx-auto bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-pink-200 to-90% rounded-3xl">
                 <Swiper
@@ -38,15 +41,17 @@ const Reviews = () => {
                                 className="my-16 mx-24 grid md:grid-cols-12 font-bold">
                                 <div className="col-span-3">
                                     <img
-                                        className="rounded-2xl"
+                                        className="rounded-2xl mx-auto"
                                         src={review.img}
                                         alt="" />
                                 </div>
                                 <div className="p-5 col-span-9">
-                                    <h2 className="text-4xl pt-2 border-t-8 border-indigo-400 w-[50%]">
+                                    <h2
+                                        className="text-4xl pt-2 border-t-8 border-indigo-400 w-[50%]">
                                         {review.name}
                                     </h2>
-                                    <div className="grid md:grid-cols-12 mt-6">
+                                    <div
+                                        className="grid md:grid-cols-12 mt-6">
                                         <div
                                             className="col-span-1">
                                             <FaQuoteLeft
@@ -54,9 +59,9 @@ const Reviews = () => {
                                         </div>
                                         <div
                                             className="col-span-10 font-bold p-5 text-slate-800 text-center">
-                                                <h2>
-                                                    {review.review}
-                                                </h2>
+                                            <h2>
+                                                {review.review}
+                                            </h2>
                                         </div>
                                         <div
                                             className="col-span-1 flex justify-end place-items-end mt-24">
