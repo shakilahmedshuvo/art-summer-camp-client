56c38674d3ef23f8b5ba51cc62dbc6ac450f7db1
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-10 18:12:20 +0600

    save the user in the db

--- src/Components/Home/SignUp/SignUp.jsx
+++ src/Components/Home/SignUp/SignUp.jsx
@@ -19,8 +19,21 @@ const SignUp = () => {
                 // console.log(user)
                 updateUserProfile(data.name, data.photoURL)
                     .then(() => {
-                        toast.success('successfully Register')
-                        navigate('/')
+                        const savedUser = { name: data.name, email: data.email }
+                        fetch('http://localhost:5000/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(savedUser)
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    toast.success('successfully Register')
+                                    navigate('/')
+                                }
+                            })
                     })
                     .catch(error => console.log(error))
             })
